032b5ddb55a4f3ace713cee2434dfc1f8deead1d
Arthur Louvrier <arthur.louvrier@etudiant.univ-lr.fr>
2020-08-13 18:50:01 +0200

    Completed (almost) the asm decoding for addressing modes

--- Sources/CPU/ASMDecode.cc
+++ Sources/CPU/ASMDecode.cc
@@ -2,15 +2,15 @@
 #include "Bus.h"
 
 #include <sstream>
+#include <iomanip>
+using std::setfill;
+using std::setw;
+using std::hex;
+using std::stringstream;
 
 void W65816::initializeAdrModeASMDecode()
 {
-    string prefix = "$";
-
-    auto absoluteAsm = [&](std::stringstream& stream)
-    {
-        stream << std::hex << prefix << getParam(1,2);
-    };
+    const string prefix = "$";
 
     //for(auto adrMode : {&Absolute,&AbsoluteWrite,&AbsoluteJMP,&AbsoluteJSR})
         //adrMode->setASMDecode(std::function<void(std::stringstream&)>(absoluteAsm)); //Or put the lambda directly here
@@ -19,12 +19,117 @@ void W65816::initializeAdrModeASMDecode()
     auto setDecoder = [&](auto decoder,std::vector<AddressingMode*> adrModeList)//, auto adrModeList)
     {
         for(auto adrMode : adrModeList)
-            adrMode->setASMDecoder(std::function<void(std::stringstream&)>(decoder));
+            adrMode->setASMDecoder(std::function<void(stringstream&)>(decoder));
     };
 
-    setDecoder(absoluteAsm,{&Absolute,&AbsoluteWrite,&AbsoluteJMP,&AbsoluteJSR});
-    setDecoder([&](std::stringstream& stream){stream << std::hex << prefix << getParam(1,3);},
+    auto prepStream = [](stringstream& stream, int width, const string && prefix = "$") -> stringstream&
+    {
+        stream << " " << prefix << hex << setfill('0') << setw(width);
+        return stream;
+    };
+
+    setDecoder([&](stringstream& stream)
+                {
+                    prepStream(stream,4) << getParam(1,2);
+                },
+                {&Absolute,&AbsoluteWrite,&AbsoluteRMW,&AbsoluteJMP,&AbsoluteJSR});
+    setDecoder([&](stringstream& stream)
+               {
+                    prepStream(stream,6) << getParam(1,3);
+               },
                {&AbsoluteLong,&AbsoluteLongWrite,&AbsoluteLongJMP,&AbsoluteLongJSL});
+    setDecoder([&](stringstream& stream)
+               {
+                    prepStream(stream,4) << getParam(1,2) << ", X";
+               },
+               {&AbsoluteX,&AbsoluteXWrite,&AbsoluteXRMW});
+    setDecoder([&](stringstream& stream)
+               {
+                    prepStream(stream,4) << getParam(1,2) << ", Y";
+               },
+               {&AbsoluteY,&AbsoluteYWrite});
+    setDecoder([&](stringstream& stream)
+               {
+                    prepStream(stream,6) << getParam(1,3) << ", X";
+               },
+               {&AbsoluteXLong,&AbsoluteXLongWrite});
+    setDecoder([&](stringstream& stream)
+               {
+                    stream << "("; prepStream(stream,4) << getParam(1,2) << ", X)";
+               },
+               {&AbsoluteXIndirectJMP,&AbsoluteXIndirectJSR});
+    setDecoder([&](stringstream& stream)
+               {
+                    stream << "("; prepStream(stream,4) << getParam(1,2) << ")";
+               },
+               {&AbsoluteIndirectJMP});
+    setDecoder([&](stringstream& stream)
+               {
+                    stream << "["; prepStream(stream,4) << getParam(1,2) << "]";
+               },
+               {&AbsoluteIndirectJML});
+    setDecoder([&](stringstream& stream)
+               {
+                    prepStream(stream,2) << getParam(1,1);
+               },
+               {&Direct,&DirectWrite,&DirectRMW});
+    setDecoder([&](stringstream& stream)
+               {
+                    stream << "("; prepStream(stream,2) << getParam(1,1) << ",X)";
+               },
+               {&DirectXIndirect,&DirectXIndirectWrite});
+    setDecoder([&](stringstream& stream)
+               {
+                    stream << "("; prepStream(stream,2) << getParam(1,1) << ")";
+               },
+               {&DirectIndirect,&DirectIndirectWrite});
+    setDecoder([&](stringstream& stream)
+               {
+                    stream << "("; prepStream(stream,2) << getParam(1,1) << "),Y";
+               },
+               {&DirectIndirectY,&DirectIndirectYWrite});
+    setDecoder([&](stringstream& stream)
+               {
+                    stream << "["; prepStream(stream,2) << getParam(1,1) << "],Y";
+               },
+               {&DirectIndirectYLong,&DirectIndirectYLongWrite});
+    setDecoder([&](stringstream& stream)
+               {
+                    stream << "["; prepStream(stream,2) << getParam(1,1) << "]";
+               },
+               {&DirectIndirectLong,&DirectIndirectLongWrite});
+    setDecoder([&](stringstream& stream)
+               {
+                    prepStream(stream,2) << getParam(1,1) << ",X";
+               },
+               {&DirectX,&DirectXWrite,&DirectXRMW});
+    setDecoder([&](stringstream& stream)
+               {
+                    prepStream(stream,2) << getParam(1,1) << ",Y";
+               },
+               {&DirectY,&DirectYWrite});
+    setDecoder([&](stringstream& stream)
+               {
+                    int data_length = 2;
+                    if(decodingTable[ir].isIndexRelated())
+                    {
+                        if(p.index8) data_length = 1;
+                    }
+                    else if(p.mem8) data_length = 1;
+                    prepStream(stream,2*data_length, "#$") << getParam(1,data_length);
+               },
+               {&Immediate,&ImmediateSpecial});
+    setDecoder([&](stringstream& stream)
+               {
+                    prepStream(stream,2) << getParam(1,1);
+               },
+               {&RelativeBranch});
+    setDecoder([&](stringstream& stream)
+               {
+                    prepStream(stream,4) << getParam(1,2);
+               },
+               {&RelativeBranchLong});
+    //todo: stack relative and block move
 }
 
 uint32_t W65816::getParam(int index, int length)
